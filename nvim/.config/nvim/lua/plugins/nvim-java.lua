7ddaec5f0b32c94679afff8623c81a8cb482eec3
Jerry Kiely <jerry@cowboysmall.com>
2025-06-29 14:46:51 +0100

    nvchad - gave up on nvim-java setup and config for now

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -1,9 +1,67 @@
 -- return {
 --
+--   "nvim-java/nvim-java",
+--   config = false,
+--   dependencies = {
+--     {
+--       "neovim/nvim-lspconfig",
+--       opts = {
+--         servers = {
+--           -- Your custom jdtls settings goes here
+--           jdtls = {
+--             settings = {
+--               java = {
+--                 configuration = {
+--                   runtimes = {
+--                     {
+--                       name = "JavaSE-21",
+--                       path = vim.fn.expand("~/.sdkman/candidates/java/current"),
+--                     },
+--                   },
+--                 },
+--               },
+--             },
+--           },
+--         },
+--         setup = {
+--           jdtls = function()
+--             -- Your custom nvim-java configuration goes here
+--             require("java").setup({
+--               jdk = {
+--                 auto_install = false,
+--               },
+--               root_markers = {
+--                 "settings.gradle",
+--                 "settings.gradle.kts",
+--                 "pom.xml",
+--                 "build.gradle",
+--                 "mvnw",
+--                 "gradlew",
+--                 "build.gradle",
+--                 "build.gradle.kts",
+--               },
+--             })
+--           end,
+--         },
+--       },
+--     },
+--   },
+-- }
+
+
+
+
+
+
+-- return {
+--
 --   {
 --     "nvim-java/nvim-java"
 --   }
 -- }
+
+
+
 
 
 -- return {
@@ -29,57 +87,3 @@
 --     },
 --   },
 -- }
-
-
-return {
-
-  "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
-
-            -- settings = {
-            --   java = {
-            --     configuration = {
-            --       runtimes = {
-            --         {
-            --           name = "JavaSE-21",
-            --           path = vim.fn.expand("~/.sdkman/candidates/java/current"),
-            --         },
-            --       },
-            --     },
-            --   },
-            -- },
-          },
-        },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              -- Your custom nvim-java configuration goes here
-
-              -- jdk = {
-              --   auto_install = false,
-              -- },
-
-              -- root_markers = {
-              --   "settings.gradle",
-              --   "settings.gradle.kts",
-              --   "pom.xml",
-              --   "build.gradle",
-              --   "mvnw",
-              --   "gradlew",
-              --   "build.gradle",
-              --   "build.gradle.kts",
-              -- },
-            })
-          end,
-        },
-      },
-    },
-  },
-}
