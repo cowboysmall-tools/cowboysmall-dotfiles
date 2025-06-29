4e63a384aa3f896e03933587b7340eec6b958777
Jerry Kiely <jerry@cowboysmall.com>
2025-06-29 14:25:07 +0100

    nvchad - nvim-java problems

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -1,48 +1,85 @@
+-- return {
+--
+--   {
+--     "nvim-java/nvim-java"
+--   }
+-- }
+
+
+-- return {
+--
+--   "nvim-java/nvim-java",
+--   dependencies = {
+--     {
+--       "neovim/nvim-lspconfig",
+--       opts = {
+--         servers = {
+--           jdtls = {
+--             -- Your custom jdtls settings goes here
+--           },
+--         },
+--         setup = {
+--           jdtls = function()
+--             require("java").setup({
+--               -- Your custom nvim-java configuration goes here
+--             })
+--           end,
+--         },
+--       },
+--     },
+--   },
+-- }
+
+
 return {
 
-  {
-    "nvim-java/nvim-java",
-    config = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        opts = {
-          servers = {
-            -- Your JDTLS configuration goes here
-            jdtls = {
-              settings = {
-                java = {
-                  configuration = {
-                    runtimes = {
-                      {
-                        name = "JavaSE-21",
-                        path = vim.fn.expand("~/.sdkman/candidates/java/current"),
-                      },
-                    },
-                  },
-                },
-              },
-            },
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            -- Your custom jdtls settings goes here
+
+            -- settings = {
+            --   java = {
+            --     configuration = {
+            --       runtimes = {
+            --         {
+            --           name = "JavaSE-21",
+            --           path = vim.fn.expand("~/.sdkman/candidates/java/current"),
+            --         },
+            --       },
+            --     },
+            --   },
+            -- },
           },
-          setup = {
-            jdtls = function()
-              -- Your nvim-java configuration goes here
-              require("java").setup({
-                root_markers = {
-                  "settings.gradle",
-                  "settings.gradle.kts",
-                  "pom.xml",
-                  "build.gradle",
-                  "mvnw",
-                  "gradlew",
-                  "build.gradle",
-                  "build.gradle.kts",
-                },
-              })
-            end,
-          },
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              -- Your custom nvim-java configuration goes here
+
+              -- jdk = {
+              --   auto_install = false,
+              -- },
+
+              -- root_markers = {
+              --   "settings.gradle",
+              --   "settings.gradle.kts",
+              --   "pom.xml",
+              --   "build.gradle",
+              --   "mvnw",
+              --   "gradlew",
+              --   "build.gradle",
+              --   "build.gradle.kts",
+              -- },
+            })
+          end,
         },
       },
     },
-  }
+  },
 }
