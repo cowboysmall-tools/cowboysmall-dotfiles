adec0526c76ab4ec89153f90a016df3ce5d2d4ab
Jerry Kiely <jerry@cowboysmall.com>
2025-08-08 23:30:47 +0100

    nvchad - trying to get nvim-java to use lombok

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -2,9 +2,9 @@ return {
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "clangd", "gopls", "jdtls", "pyright", "rust-analyzer" },
-    },
+    -- opts = {
+    --   ensure_installed = { "clangd", "gopls", "jdtls", "pyright", "rust-analyzer" },
+    -- },
     dependencies = {
       {
         "mason-org/mason.nvim",
@@ -12,6 +12,13 @@ return {
           registries = {
             "github:nvim-java/mason-registry",
             "github:mason-org/mason-registry",
+          },
+          ensure_installed = {
+            "clangd",
+            "gopls",
+            "jdtls",
+            "pyright",
+            "rust-analyzer",
           },
         }
       },
--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -1,95 +1,22 @@
 return {
 
-  -- "nvim-java/nvim-java",
-  -- lazy = false,
-  -- config = false,
+  "nvim-java/nvim-java",
+  lazy = false,
+  config = false,
 
-  -- dependencies = {
-  --   {
-  --     "neovim/nvim-lspconfig",
-  --     opts = {
-  --       servers = {
-  --         jdtls = {
-  --           -- settings = {
-  --           --   java = {
-  --           --     configuration = {
-  --           --       runtimes = {
-  --           --         {
-  --           --           name = "JavaSE-21",
-  --           --           path = "/home/jerry/.sdkman/candidates/java/current",
-  --           --         },
-  --           --       },
-  --           --     },
-  --           --   },
-  --           -- },
-  --         },
-  --       },
-  --       setup = {
-  --         jdtls = function()
-  --           require("java").setup({
-
-  --             -- root_markers = {
-  --             --   "settings.gradle",
-  --             --   "settings.gradle.kts",
-  --             --   "pom.xml",
-  --             --   "build.gradle",
-  --             --   "mvnw",
-  --             --   "gradlew",
-  --             --   "build.gradle",
-  --             --   "build.gradle.kts",
-  --             --   ".git",
-  --             -- },
-
-  --             -- jdtls = {
-  --             --   enable = true,
-  --             --   version = "v1.48.0",
-  --             -- },
-
-  --             -- lombok = {
-  --             --   enable = true,
-  --             --   version = "nightly",
-  --             -- },
-
-  --             -- java_test = {
-  --             --   enable = true,
-  --             -- },
-
-  --             -- java_debug_adapter = {
-  --             --   enable = true,
-  --             -- },
-
-  --             -- spring_boot_tools = {
-  --             --   enable = true,
-  --             -- },
-
-  --             -- jdk = {
-  --             --   auto_install = false,
-  --             -- },
-
-  --             -- notifications = {
-  --             --   dap = true,
-  --             -- },
-
-  --             -- verification = {
-  --             --   invalid_order = true,
-  --             --   duplicate_setup_calls = true,
-  --             --   invalid_mason_registry = false,
-  --             -- },
-
-  --             -- mason = {
-  --             --   registries = {
-  --             --     "github:nvim-java/mason-registry",
-  --             --   },
-  --             -- },
-
-  --           })
-
-  --           require("lspconfig").jdtls.setup({})
-
-  --         end,
-
-  --       },
-  --     },
-  --   },
-  -- },
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {},
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({})
+          end,
+        },
+      },
+    },
+  },
 }
