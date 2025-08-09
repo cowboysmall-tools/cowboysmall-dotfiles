06d8f3dd90418f93a6469a77e8481308ad54aab8
Jerry Kiely <jerry@cowboysmall.com>
2025-08-09 08:25:19 +0100

    nvchad - added some comments to a plugin

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -1,7 +1,7 @@
 return {
 
   "nvim-java/nvim-java",
-  lazy = false,
+  -- lazy = false,
   config = false,
 
   dependencies = {
@@ -9,11 +9,20 @@ return {
       "neovim/nvim-lspconfig",
       opts = {
         servers = {
-          jdtls = {},
+          jdtls = {
+            -- I previously added config
+            -- for the version of the
+            -- current jdk installed and
+            -- configured through sdkman
+          },
         },
         setup = {
           jdtls = function()
-            require("java").setup({})
+            require("java").setup({
+              -- I previously added config
+              -- for lombok here, explicitly
+              -- specifying the version, etc.
+            })
           end,
         },
       },
