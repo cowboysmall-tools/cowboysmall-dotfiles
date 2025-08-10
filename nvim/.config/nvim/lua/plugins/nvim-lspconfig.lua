e180e0a43869fd77bb834efc1b0cc48e6d4dd75a
Jerry Kiely <jerry@cowboysmall.com>
2025-08-10 15:01:33 +0100

    nvchad - added a comment to the lspconfig plugin

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,11 @@ return {
       require('java').setup({
 
         -- Your custom jdtls settings goes here
-        root_markers = {".git" },
+        root_markers = { ".git" },
+
+        -- this approach does not seem to work
+        -- root_dir = vim.loop.cwd(),
+        -- root_dir = vim.fn.getcwd(),
       })
 
       require "configs.lspconfig"
