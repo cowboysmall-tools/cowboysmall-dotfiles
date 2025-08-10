00e04597d9713788d586d223efcfd02642a34737
Jerry Kiely <jerry@cowboysmall.com>
2025-08-10 01:15:18 +0100

    nvchad - changed plugin config

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,41 +7,7 @@ return {
       require('java').setup({
 
         -- Your custom jdtls settings goes here
-
-        root_markers = {
-          ".git",
-          -- "mvnw",
-          -- "pom.xml",
-          -- "gradlew",
-          -- "settings.gradle",
-          -- "settings.gradle.kts",
-          -- "build.gradle",
-          -- "build.gradle.kts",
-        },
-
-        -- jdtls = {
-        --   enable = false,
-        -- },
-
-        -- lombok = {
-        --   version = "nightly",
-        -- },
-
-        -- java_test = {
-        --   enable = false,
-        -- },
-
-        -- java_debug_adapter = {
-        --   enable = false,
-        -- },
-
-        -- spring_boot_tools = {
-        --   enable = false,
-        -- },
-
-        -- jdk = {
-        --   auto_install = false,
-        -- },
+        root_markers = {".git" },
       })
 
       require "configs.lspconfig"
