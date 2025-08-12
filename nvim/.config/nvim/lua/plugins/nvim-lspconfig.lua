90d9aefacdb16a775eb239450c662602c6efaf28
Jerry Kiely <jerry@cowboysmall.com>
2025-08-12 01:04:59 +0100

    updated plugins

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,7 @@ return {
       require('java').setup({
 
         -- Your custom jdtls settings goes here
-        root_markers = { ".git" },
+        root_markers = { ".git", "mvnw", "gradlew" },
 
         -- this approach does not seem to work
         -- root_dir = vim.loop.cwd(),
