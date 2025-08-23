74568ed7cbead767f39c1b2d800c8233f0652973
Jerry Kiely <jerry@cowboysmall.com>
2025-08-23 12:25:30 +0100

    nvchad - restored two autocmds

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,7 +1,9 @@
 require "nvchad.autocmds"
 
+local autocmd = vim.api.nvim_create_autocmd
 
-vim.api.nvim_create_autocmd("LspAttach", {
+
+autocmd("LspAttach", {
 
   callback = function(event)
     local bufmap = function(mode, rhs, lhs)
@@ -31,4 +33,32 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- bufmap("n", "<C-M-k>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
   end
 
+})
+
+
+
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
+
+
+autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
 })
