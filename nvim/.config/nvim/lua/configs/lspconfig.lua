ce70d75b84eacc70cec5b51a0233b497afdc4cb6
Jerry Kiely <jerry@cowboysmall.com>
2025-08-26 17:40:23 +0100

    nvchad - further cleanup of configs + changed some mappings from shift to meta

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "gopls", "pyright" }
-vim.lsp.enable(servers)
+-- local servers = { "html", "cssls" }
+-- vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
+
+require("lspconfig").html.setup({})
+require("lspconfig").cssls.setup({})
+require("lspconfig").clangd.setup({})
+require("lspconfig").gopls.setup({})
+require("lspconfig").pyright.setup({})
 
 require("lspconfig").jdtls.setup({
   settings = {
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,33 +6,36 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- basic editor commands
+-- basic nvim commands
+map("n", "<S-w>",     "<cmd>w<CR>",   { desc = "write buffer",   remap = true })
+map("n", "<S-Left>",  "<C-h>",        { desc = "left panel",     remap = true })
+map("n", "<S-Right>", "<C-l>",        { desc = "right panel",    remap = true })
+map("n", "<S-c>",     "<cmd>ccl<CR>", { desc = "close quickfix", remap = true })
+map("n", "<S-q>",     "<cmd>q<CR>",   { desc = "quit neovim",    remap = true })
+
+-- tab operations
 local tabufline = require("nvchad.tabufline")
 local next_tab  = function() tabufline.next() end
 local prev_tab  = function() tabufline.prev() end
 local close_tab = function() tabufline.close_buffer() end
 
-map("n", "<M-q>",        "<cmd>q<CR>",   { desc = "quit neovim",    remap = true })
-map("n", "<M-w>",        "<cmd>w<CR>",   { desc = "write buffer",   remap = true })
-map("n", "<M-c>",        "<cmd>ccl<CR>", { desc = "close quickfix", remap = true })
-map("n", "<M-Left>",     "<C-h>",        { desc = "left panel",     remap = true })
-map("n", "<M-Right>",    "<C-l>",        { desc = "right panel",    remap = true })
-map("n", "<M-PageDown>", next_tab,       { desc = "next tab",       remap = true })
-map("n", "<M-PageUp>",   prev_tab,       { desc = "previous tab",   remap = true })
-map("n", "<M-x>",        close_tab,      { desc = "close tab",      remap = true })
+map("n", "<S-PageDown>", next_tab,  { desc = "next tab",     remap = true })
+map("n", "<S-PageUp>",   prev_tab,  { desc = "previous tab", remap = true })
+map("n", "<S-x>",        close_tab, { desc = "close tab",    remap = true })
 
--- new file
-local inlay_hint        = vim.lsp.inlay_hint
-local toggle_inlay_hint = function() inlay_hint.enable(not inlay_hint.is_enabled()) end
-
-map("n", "<S-n>", "<cmd>NewFile<CR>",              { desc = "new file",      remap = true })
-map("n", "<S-h>", "<cmd>NewFileHere<CR>",          { desc = "new file here", remap = true })
-map("n", "<S-f>", "<cmd>Telescope find_files<CR>", { desc = "telescope",     remap = true })
-map("n", "<S-i>", toggle_inlay_hint,               { desc = "toggle hints",  remap = true })
-
--- cut, copy, and paste
+-- editor operations (cut, copy, paste, and comment)
 map("v", "<C-d>", "\"+d", { desc = "cut",     remap = true })
 map("v", "<C-y>", "\"+y", { desc = "copy",    remap = true })
 map("n", "<C-p>", "\"+p", { desc = "paste",   remap = true })
 map("v", "<C-_>", "gc",   { desc = "comment", remap = true })
 map("n", "<C-_>", "gcc",  { desc = "comment", remap = true })
+
+-- file operations
+map("n", "<M-n>", "<cmd>NewFile<CR>",              { desc = "new file",      remap = true })
+map("n", "<M-h>", "<cmd>NewFileHere<CR>",          { desc = "new file here", remap = true })
+map("n", "<M-f>", "<cmd>Telescope find_files<CR>", { desc = "telescope",     remap = true })
+
+-- toggle inlay hints
+local inlay_hint        = vim.lsp.inlay_hint
+local toggle_inlay_hint = function() inlay_hint.enable(not inlay_hint.is_enabled()) end
+map("n", "<M-i>", toggle_inlay_hint, { desc = "toggle hints",  remap = true })
