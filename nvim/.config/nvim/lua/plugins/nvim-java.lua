2f8d2cb19b61cb16e5b962fc5fc096c45c648148
Jerry Kiely <jerry@cowboysmall.com>
2025-09-02 21:14:13 +0100

    nvchad - updated java config

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -6,11 +6,22 @@ return {
 
     config = function()
       require("java").setup({
+
         root_markers = {
           "mvnw",
           "gradlew",
           ".git"
         }
+
+        -- java_test = {
+        --   enable = true,
+        --   version = "0.43.1",
+        -- }
+
+        -- spring_boot_tools = {
+        --   enable = true,
+        --   version = "1.59.0",
+        -- }
       })
     end
   }
