c8da9abd1cd2a02da8fb92248d89fdc343e1dbbf
Jerry Kiely <jerry@cowboysmall.com>
2025-09-18 20:05:38 +0100

    nvchad - cleaned up lspconfig config file

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,18 +1,30 @@
 require("nvchad.configs.lspconfig").defaults()
 
+
 local servers = { "clangd", "cssls", "gopls", "html", "jsonls", "pyright", "texlab", "vtsls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers
 
--- require("lspconfig").clangd.setup({})
--- require("lspconfig").cssls.setup({})
--- require("lspconfig").gopls.setup({})
--- require("lspconfig").html.setup({})
--- require("lspconfig").jsonls.setup({})
--- require("lspconfig").pyright.setup({})
--- require("lspconfig").texlab.setup({})
--- require("lspconfig").vtsls.setup({})
+-- vim.lsp.enable("jdtls")
+-- vim.lsp.config("jdtls", {
+
+--   settings = {
+--     java = {
+--       configuration = {
+--         runtimes = {
+--           {
+--             name = "JavaSE-21",
+--             path = "/home/jerry/.sdkman/candidates/java/current",
+--             default = true
+--           }
+--         }
+--       }
+--     }
+--   }
+-- })
+
+
+-- read :h vim.lsp.config for changing options of lsp servers
 
 require("lspconfig").jdtls.setup({
 
