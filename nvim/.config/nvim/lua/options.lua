2244396a653c040e4c0ece96e0dba6b6754b4195
Jerry Kiely <jerry@cowboysmall.com>
2025-09-28 21:26:24 +0100

    nvchad - disabled some options - look for a better solution to setting tabstops, etc.

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,9 +6,9 @@ local opt = vim.opt
 local o   = vim.o
 local g   = vim.g
 
-o.tabstop     = 4
-o.shiftwidth  = 4
-o.softtabstop = 4
+-- o.tabstop     = 4
+-- o.shiftwidth  = 4
+-- o.softtabstop = 4
 
 o.cursorlineopt = "both" -- to enable cursorline!
 
