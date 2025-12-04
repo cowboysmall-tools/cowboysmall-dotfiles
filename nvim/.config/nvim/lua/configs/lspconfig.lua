42c9fe94a4dd888e2eafebb9203cc1cf3b0fdfd0
Jerry Kiely <jerry@cowboysmall.com>
2025-12-04 00:44:26 +0000

    nvchad - updated nvim-java

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,33 +1,13 @@
 require("nvchad.configs.lspconfig").defaults()
 
 
-local servers = { "clangd", "cssls", "gopls", "html", "jsonls", "pyright", "texlab", "vtsls" }
+-- local servers = { "clangd", "cssls", "gopls", "html", "jsonls", "pyright", "texlab", "vtsls" }
+local servers = { "clangd", "cssls", "gopls", "html", "jdtls", "jsonls", "pyright", "texlab", "vtsls" }
 vim.lsp.enable(servers)
 
 
 -- vim.lsp.enable("jdtls")
--- vim.lsp.config("jdtls", {
-
---   settings = {
---     java = {
---       configuration = {
---         runtimes = {
---           {
---             name = "JavaSE-21",
---             path = "/home/jerry/.sdkman/candidates/java/current",
---             default = true
---           }
---         }
---       }
---     }
---   }
--- })
-
-
--- read :h vim.lsp.config for changing options of lsp servers
-
-require("lspconfig").jdtls.setup({
-
+vim.lsp.config("jdtls", {
   settings = {
     java = {
       configuration = {
@@ -42,3 +22,6 @@ require("lspconfig").jdtls.setup({
     }
   }
 })
+
+
+-- read :h vim.lsp.config for changing options of lsp servers
--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -4,15 +4,32 @@ return {
     "nvim-java/nvim-java",
 
     config = function()
-      require("java").setup({
 
+      require("java").setup({
         root_markers = {
           "mvnw",
           "gradlew",
           ".git"
         }
       })
+
+      -- vim.lsp.enable("jdtls")
+      -- vim.lsp.config("jdtls", {
+      --   settings = {
+      --     java = {
+      --       configuration = {
+      --         runtimes = {
+      --           {
+      --             name = "JavaSE-21",
+      --             path = "/home/jerry/.sdkman/candidates/java/current",
+      --             default = true
+      --           }
+      --         }
+      --       }
+      --     }
+      --   }
+      -- })
+
     end
   }
-
 }
