2c6a982b9321f32c3b44e7f7e0a718492f29ac5e
Jerry Kiely <jerry@cowboysmall.com>
2026-03-14 11:43:03 +0000

    nvim-java - updated config

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -15,21 +15,21 @@ return {
       })
 
       vim.lsp.enable("jdtls")
-      vim.lsp.config("jdtls", {
-        settings = {
-          java = {
-            configuration = {
-              runtimes = {
-                {
-                  name = "JavaSE-21",
-                  path = "/home/jerry/.sdkman/candidates/java/current",
-                  default = true
-                }
-              }
-            }
-          }
-        }
-      })
+      -- vim.lsp.config("jdtls", {
+      --   settings = {
+      --     java = {
+      --       configuration = {
+      --         runtimes = {
+      --           {
+      --             name = "JavaSE-21",
+      --             path = "/home/jerry/.sdkman/candidates/java/current",
+      --             default = true
+      --           }
+      --         }
+      --       }
+      --     }
+      --   }
+      -- })
 
     end
   }
